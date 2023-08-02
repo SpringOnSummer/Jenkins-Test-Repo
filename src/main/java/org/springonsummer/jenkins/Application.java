4fb9a5d820f556b3535db9d9dfc15a266b74bd65
SEONG JUN LEE <devel0624@gmail.com>
2023-08-02 15:19:39 +0900

    convert to spring boot application.

--- src/main/java/org/springonsummer/jenkins/Application.java
+++ src/main/java/org/springonsummer/jenkins/Application.java
@@ -1,7 +1,11 @@
 package org.springonsummer.jenkins;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Jenkins Test!");
+        SpringApplication.run(Application.class, args);
     }
 }
